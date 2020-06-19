a3c25c3c3b80e28e703aeeb82d802d5f2b59f606
Luis <gerard95@live.com.mx>
2020-06-18 23:37:13 -0500

    se corrige la ruta del endpoint

--- src/services/superffService.js
+++ src/services/superffService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 /* const ENDPOINT = 'https://superflyfitnessapi.herokuapp.com'; */
-const ENDPOINT = 'https://superflyfitnessapi.herokuapp.com/';
+const ENDPOINT = 'https://superflyfitnessapi.herokuapp.com';
 
 const config = {
   headers: {
